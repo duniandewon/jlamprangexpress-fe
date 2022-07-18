a78dd6099e3632fa9997dd3fe96533af5d941480
aboli <duniandewon@gmail.com>
2022-07-18 15:29:59 +0700

    Wrap with DashboardLayout

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -12,25 +12,6 @@ export const axiosPrivate = axios.create({
   withCredentials: true,
 });
 
-export const privateFetch = (options) =>
-  new Promise((resolve, reject) => {
-    axiosPrivate(options)
-      .then((res) => {
-        resolve(res.data);
-      })
-      .catch((err) => {
-        const defaultError = {
-          code: 500,
-          status: "error",
-          message: "Failed to fetch data. Please contact developer.",
-        };
-
-        if (typeof err.response === "undefined") reject(defaultError);
-        else if (typeof err.response.data === "undefined") reject(defaultError);
-        else reject(err.response.data);
-      });
-  });
-
 export default (options) =>
   new Promise((resolve, reject) => {
     fetch(options)
